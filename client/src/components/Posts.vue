265d3d413494bb03387b0b77ca76e89fe87d6039
Kenny Devin <kendevxz@gmail.com>
2024-04-06 17:05:16 +0700

    rails vue posts done

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -17,6 +17,15 @@
 
     <!-- only render if not editing post -->
     <button v-else @click="createPost">Create</button>
+
+    <!-- list of posts -->
+    <div v-for="post in posts" :key="post.id">
+        <h5>[{{post.id}}] {{ post.title }}</h5>
+        <p>{{ post.body }}</p>
+
+        <button @click="editPost(post.id)">Edit</button>
+        <button @click="deletePost(post.id)">Delete</button>
+    </div>
   </div>
 </template>
 
@@ -24,39 +33,87 @@
   import { ref, onMounted } from 'vue';
 
   const posts = ref([])
-
   const title = ref('')
   const body = ref('')
   const post_id = ref(0)
   const isEditing = ref('')
   const API_URL = 'http://localhost:3000/posts'
 
+  onMounted(async() => {
+    const res = await fetch(API_URL)
+    posts.value = await res.json()
+  })
+
   const createPost = async() => {
-      const res = await fetch(API_URL, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            title: title.value,
-            body: body.value
-          })
+    const res = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: title.value,
+        body: body.value
       })
+    })
 
-      const data = await res.json()
+    const data = await res.json()
 
-      posts.value.push(data)
-      title.value = ''
-      body.value = ''
-      post_id.value = 0;
+    posts.value.push(data)
+    title.value = ''
+    body.value = ''
+    post_id.value = 0;
   }
 
   const updatePost = async() => {
-    return true;
+    const res = await fetch(`${API_URL}/${post_id.value}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: title.value,
+        body: body.value,
+        id: post_id.value
+      })
+    })
+
+    const data = await res.json()
+
+    const index = posts.value.findIndex(post => post.id === data.id)
+    posts.value[index] = data
+
+    title.value = ''
+    body.value = ''
+    post_id.value = 0;
+    isEditing = false
   }
 
   const cancelEdit = () => {
-    return true;
+    title.value = ''
+    body.value = ''
+    post_id.value = 0
+    isEditing.value = false
+  }
+
+  const deletePost = async(id) => {
+    await fetch (`${API_URL}/${id}`, {
+      method: 'DELETE'
+    })
+    posts.value = posts.value.filter(post => post.id !== id)
+  }
+
+  const editPost = async(id) => {
+    const post = posts.value.find(post => post.id === id)
+
+    title.value = post.title
+    body.value = post.body
+    post_id.value = post.id
+    isEditing.value = true
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
   }
 </script>
 
